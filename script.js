112a8e3d7cd02a432315bc975013a3db239055bc
Andrey L <newman4525@gmail.com>
2022-04-03 22:51:51 +0300

    hardLess04iscComplite

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let variable = 'true';
+
+
+function showTypeof(value){
+	console.log(value, typeof value, value.length)
+}
+
+function getString(value){
+if(typeof value !== 'string'){
+	return 'Это не строка!'
+}else{
+	let operand = value.trim();
+
+if(operand.length > 30){
+	return operand.replace(operand.substring(30),'...')
+}else{
+	return operand
+	
+}}}
+
+
+showTypeof(variable)
+showTypeof(getString(variable))
