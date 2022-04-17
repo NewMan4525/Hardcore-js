b4270225541f1725429f925a0bfa08d999d67a92
Andrey L <newman4525@gmail.com>
2022-04-17 06:07:16 +0300

    hard_09complite

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ const ul = document.createElement('ul');
 const liA = document.createElement('li');
 const liB = document.createElement('li');
 const styleAll = document.createElement('style');
-let date = new Date();
+let date;
 
 const week = [
 	'Воскресенье', 'Понедельник', 'Вторник',
 	'Среда', 'Четверг', 'Пятница',
 	'Суббота'
-
 ];
 const month = [
 	'января', 'февраля', 'марта',
@@ -20,9 +19,6 @@ const month = [
 	'октября', 'ноября', 'декабря'
 ];
 
-
-body.prepend(ul, styleAll);
-ul.prepend(liA, liB);
 
 function rusNotation(value, type) {
 
@@ -58,9 +54,7 @@ function rusNotation(value, type) {
 			return currentType[0];
 		} else {
 			return currentType[2];
-			//11,12,13,14 часов
-			//11,12,13,14 минут
-			//11,12,13,14 секунд
+			//11,12,13,14 часов /минут /секунд
 		}
 	} else {
 
@@ -70,7 +64,7 @@ function rusNotation(value, type) {
 			case 2:
 			case 3:
 			case 4:
-				return currentType[1]; //часа//12часов
+				return currentType[1]; //часа
 			case 5:
 			case 6:
 			case 7:
@@ -82,41 +76,43 @@ function rusNotation(value, type) {
 	}
 }
 
-styleAll.textContent = 'ul{width: 500px;height: 100px;background-color:grey;' +
-	'margin:20px auto;display:flex;flex-direction:column;color:white;}' +
-	'li{margin-top: 20px;}';
+function renderDate(value) {
 
-liA.textContent = 'Сегодня' +
-	' ' + week[date.getDay()] +
-	',' + ' ' + date.getDate() +
-	' ' + month[date.getMonth()] +
-	' ' + date.getFullYear() + ' ' + 'года' + ',' + ' ' +
-	' ' + date.getHours() + ' ' + rusNotation(date.getHours(), 'h') +
-	' ' + date.getMinutes() + ' ' + rusNotation(date.getMinutes(), 'm') +
-	' ' + date.getSeconds() + ' ' + rusNotation(date.getSeconds(), 's');
+	if (value < 10) {
+		value = '0' + value;
+	}
+	return value;
+}
 
-liB.textContent = 'none';
-
-setInterval(rusNotation, 1000);
-
-
-
-
-// //1, 21 час //h=1||h=24
-// 2, 3, 4, 22, 23, 24 часа //h>1||h<5;h>21||h<25
-// 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 часов //h===0;h>4||h<20
+function timeUpdata() {
+	date = new Date();
+	liA.textContent = 'Сегодня' +
+		' ' + week[date.getDay()] +
+		',' + ' ' + date.getDate() +
+		' ' + month[date.getMonth()] +
+		' ' + date.getFullYear() + ' ' + 'года' + ',' + ' ' +
+		' ' + date.getHours() + ' ' + rusNotation(date.getHours(), 'h') +
+		' ' + date.getMinutes() + ' ' + rusNotation(date.getMinutes(), 'm') +
+		' ' + date.getSeconds() + ' ' + rusNotation(date.getSeconds(), 's');
 
 
-// 1) Выведите на страницу текущую дату и время в 2-х форматах:
+	liB.textContent =
+		renderDate(date.getDate()) +
+		'.' + renderDate(date.getMonth()) +
+		'.' + date.getFullYear() +
+		'-' + renderDate(date.getHours()) +
+		':' + renderDate(date.getMinutes()) +
+		':' + renderDate(date.getSeconds());
 
-//     a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
-//     б) '04.02.2020 - 21:05:33'
+}
 
-// 2) Для вывода в формате (а) напишите функцию,
-//    которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
+body.prepend(ul, styleAll);
+ul.prepend(liA, liB);
 
-// 3) Для вывода в формате (б) напишите функцию, которая будет добавлять 0
-//    перед значениями которые состоят из одной цифры
-//    (из 9:5:3 1.6.2019 сделает 09:05:03 01.06.2019)
+styleAll.textContent =
+	'ul{width: 550px;height: 100px;background-color:grey;padding:0;' +
+	'margin:20px auto;list-style:none;display:flex;flex-direction:column;color:white;}' +
+	'li{margin:12px;}';
 
-// 4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду
+timeUpdata();
+setInterval(timeUpdata, 1000);
