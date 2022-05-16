bdec38921664715da61f96a5ce176cbdf67a34eb
Andrey L <newman4525@gmail.com>
2022-05-17 01:54:40 +0300

    hardlLess19IsComplite

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
 'use strict';
+
+const div = document.querySelector('div');
+const input = div.querySelector('input');
+const par = div.querySelector('p');
+const btnStrt = div.querySelector('.strt');
+const btnRes = div.querySelector('.rst');
+
+const getTime = () => {
+	let timer = 3000;
+	let delayProcess;
+
+
+	const callBack = () => {
+
+		par.textContent = input.value;
+	};
+
+	function debounce(fn, interval) {
+
+		let timer;
+		return function debounced() {
+
+			clearTimeout(timer);
+			let args = arguments;
+			let that = this;
+			timer = setTimeout(function callOriginalFn() {
+				fn.apply(that, args);
+			}, interval);
+		};
+	}
+
+	delayProcess = debounce(callBack, timer);
+	input.addEventListener('input', delayProcess);
+
+};
+
+function animEEEE() {
+
+	let active;
+	let idrequest;
+	let degres = 0;
+
+	const animateInput = () => {
+
+		degres++;
+
+		input.style.cssText = `transform: rotate(${degres}deg)`;
+		idrequest = requestAnimationFrame(animateInput);
+
+	};
+
+	const reset = () => {
+
+		cancelAnimationFrame(idrequest);
+		degres = 0;
+		input.style.cssText = `transform: rotate(0 deg)`;
+
+	};
+
+	btnStrt.addEventListener('click', () => {
+
+		if (active) {
+
+			cancelAnimationFrame(idrequest);
+			active = false;
+
+		} else {
+
+			idrequest = requestAnimationFrame(animateInput);
+			active = true;
+
+		}
+
+	});
+
+
+	btnRes.addEventListener('click', reset);
+	input.addEventListener('click', reset);
+
+}
+
+
+getTime();
+animEEEE();
